ce00cdf36c8516166a6880e2a4d6978755d30611
manojlamichhane <lamichhanemanoj1@gmail.com>
2021-03-03 19:23:19 +0545

    sd

--- navigation/RootNavigator.js
+++ navigation/RootNavigator.js
@@ -3,6 +3,7 @@ import { Text, View } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import InboxScreen from "../Screens/InboxScreen";
 import OutboxScreen from "../Screens/OutboxScreen";
@@ -10,12 +11,13 @@ import TrashScreen from "../Screens/TrashScreen";
 import SpamScreen from "../Screens/SpamScreen";
 
 const Drawer = createDrawerNavigator();
+const InboxStack = createStackNavigator();
 
 const RootNavigator = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
-        <Drawer.Screen name="Inbox" component={InboxScreen} />
+        <Drawer.Screen name="Inbox" component={InboxStackNavigator} />
         <Drawer.Screen name="Outbox" component={OutboxScreen} />
         <Drawer.Screen name="Trash" component={TrashScreen} />
         <Drawer.Screen name="Spam" component={SpamScreen} />
@@ -24,4 +26,11 @@ const RootNavigator = () => {
   );
 };
 
+const InboxStackNavigator = () => {
+  return (
+    <InboxStack.Navigator>
+      <InboxStack.Screen name="Inbox" component={InboxScreen} />
+    </InboxStack.Navigator>
+  );
+};
 export default RootNavigator;
